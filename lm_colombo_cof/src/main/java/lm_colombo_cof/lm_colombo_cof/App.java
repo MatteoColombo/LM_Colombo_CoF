405c8c731fd4b6849097dbd05d4fbb09d33b4d14
Matteo Colombo <matte.colo@gmail.com>
2016-04-19 22:16:25 +0200

    Maven project created

---
+++ lm_colombo_cof/src/main/java/lm_colombo_cof/lm_colombo_cof/App.java
@@ -0,0 +1,13 @@
+package lm_colombo_cof.lm_colombo_cof;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

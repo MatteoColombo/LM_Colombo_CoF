f340d3a43c7464ec7b6288caa7642451e0b04bcd
Matteo Colombo <matte.colo@gmail.com>
2016-05-10 16:14:40 +0200

    Created City Class

---
+++ lm_colombo_cof/src/main/java/game/City.java
@@ -0,0 +1,54 @@
+package game;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class City {
+	private String cityName;
+	private Color cityColor;
+	// private Reward cityReward;
+	private boolean hasKing;
+	private ArrayList<Emporium> emporiums;
+
+	public City(Color cityColor, String cityName, boolean hasKing /* Reward cityReward */) {
+		this.cityName = cityName;
+		this.cityColor = cityColor;
+		this.hasKing = hasKing;
+		this.emporiums= new ArrayList<Emporium>();
+		//this.cityReward=cityReward;
+	}
+
+	public City(Color cityColor, String cityName /*, Reward cityReward*/) {
+		this.cityName = cityName;
+		this.cityColor = cityColor;
+		this.hasKing = false;
+		//this.cityReward=cityReward;
+	}
+
+	public String getName() {
+		return cityName;
+	}
+
+	public Color getColor() {
+		return cityColor;
+	}
+	
+	public void addEmporium(Emporium e){
+		this.emporiums.add(e);
+	}
+	//I need to pass the player
+	public boolean hasEmporiumOfPlayer(){
+		for(Emporium e: emporiums){
+			//I need to check if it exists an emporium of the received player
+			if(true)
+				return true;
+		}
+		return false;
+	}
+	
+	/*
+	private Reward getReward(){
+		return cityReward;
+	}*/
+
+}

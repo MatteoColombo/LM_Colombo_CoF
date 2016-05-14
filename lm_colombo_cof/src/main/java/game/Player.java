fdde0c58c2694ef5f558e38ad83f2e684fdab187
Davide Cavallini <Davide@10.170.12.212>
2016-05-14 17:54:54 +0200

    Created Player //missing 3 classes

--- lm_colombo_cof/src/main/java/game/Player.java
+++ lm_colombo_cof/src/main/java/game/Player.java
@@ -1,5 +1,43 @@
 package game;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	private Coins coins;
+	private Assistants assistants;
+	private VictoryPoints victoryPoints;
+	private NoblePoints noblePoints;
+	private ArrayList<PoliticCard> politicCard;
+	private ArrayList<PermissionCard> permissionCard;
+	private ArrayList<Emporium> emporium;
+
+	public Player(int money, int helper, int draw, int maxEmp) {
+		this.coins = new Coins(money);
+		this.assistants = new Assistants(helper);
+		this.victoryPoints = new VictoryPoints(0);
+		this.noblePoints = new NoblePoints(0);
+		this.politicCard = new ArrayList<PoliticCard>();
+		this.permissionCard = new ArrayList<PermissionCard>();
+		this.emporium = new ArrayList<Emporium>();
+		for (int i = 0; i < draw; i++)
+			politicCard.add(new PoliticCard());
+		for (int i = 0; i < maxEmp; i++)
+			emporium.add(new Emporium(this));
+	}
+
+	public int getCoins() {
+		return this.coins.getAmount();
+	}
+
+	public int getAssistants() {
+		return this.assistants.getAmount();
+	}
+
+	public int getVictoryPoints() {
+		return this.victoryPoints.getAmount();
+	}
+
+	public int getNoblePoints() {
+		return this.noblePoints.getAmount();
+	}
 }

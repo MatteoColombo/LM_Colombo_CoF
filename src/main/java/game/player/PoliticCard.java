fa67175675c5f3b77e5eacb38fec7a32d6608926
Matteo Colombo <matte.colo@gmail.com>
2016-05-19 12:05:19 +0200

    Added the possibility to have a multiplecolor card

--- src/main/java/game/player/PoliticCard.java
+++ src/main/java/game/player/PoliticCard.java
@@ -6,13 +6,24 @@ import java.util.Random;
 
 public class PoliticCard {
 	private Color color;
+	private boolean isMultipleColor;
 
-	public PoliticCard(ArrayList<Color> pickedColours) { // still pending if Array or List of String
-		int i = new Random().nextInt(pickedColours.size());
-		this.color = pickedColours.get(i); // if color==null, then it's the
-															// multicolor one that it has been picked
+	public PoliticCard(ArrayList<Color> possibleColors) {
+		Random r = new Random();
+		int i = r.nextInt(possibleColors.size() + 1);
+		if(i== possibleColors.size())
+			isMultipleColor=true;
+		else{
+			isMultipleColor=false;
+			color= possibleColors.get(i);
+		}
 	}
 
+
+	public boolean isMultipleColor(){
+		return isMultipleColor;
+	}
+	
 	public Color getCardColor() {
 		return this.color;
 	}

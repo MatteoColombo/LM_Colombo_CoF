6e807c1590709618b6914f25f36ef8a70fcb3a70
Matteo Colombo <matte.colo@gmail.com>
2016-05-20 14:28:26 +0200

    Map explorer implemented

--- src/main/java/game/board/MapExplorer.java
+++ src/main/java/game/board/MapExplorer.java
@@ -1,8 +1,9 @@
-package game.board.city;
+package game.board;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import game.board.city.City;
 import game.player.Player;
 import game.reward.Reward;
 

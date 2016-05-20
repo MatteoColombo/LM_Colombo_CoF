e77032508b9b30eb74941e4166824f9e387518cf
Matteo Colombo <matte.colo@gmail.com>
2016-05-20 17:28:52 +0200

    Javadoc and bugfixes

--- src/main/java/game/board/MapExplorer.java
+++ src/main/java/game/board/MapExplorer.java
@@ -16,11 +16,16 @@ public class MapExplorer {
 		this.connectedCities = new ArrayList<>();
 	}
 
+	/**
+	 * Iteratively searches for the cities that have an emporium of the player and that are part of a continous path
+	 * @param startingCity the root of the path
+	 * @param p the player who owns the emporiums
+	 * @return a list of the rewards
+	 */
 	public List<Reward> getAdiacentRewards(City startingCity, Player p) {
 		resetAdiacents();
-		List<Reward> rewards = new ArrayList<>();
-		if (!startingCity.isCapital())
-			rewards.add(startingCity.getReward());
+		List<Reward> rewards = new ArrayList<>();;
+		connectedCities.add(startingCity);
 		for (City temp : connectedCities) {
 			if (temp.hasEmporiumOfPlayer(p)) {
 				adiacentCities.add(temp);
@@ -32,6 +37,10 @@ public class MapExplorer {
 		return rewards;
 	}
 
+	/**
+	 * Adds to the list of the cities that have to be visited those adiacent to the one received
+	 * @param root the root of the sub path
+	 */
 	private void addConnectedCities(City root) {
 		List<City> adiacentsToRoot = root.getConnectedCities();
 		for (City temp : adiacentsToRoot) {
@@ -40,7 +49,12 @@ public class MapExplorer {
 		}
 
 	}
-
+	/**
+	 * Checks if the city received as parameter is already in the list of the city which has to be visited. 
+	 * This is used to avoid loops 
+	 * @param c
+	 * @return true if it is already in the list, false otherwise
+	 */
 	private boolean checkIfInList(City c){
 		for(City temp: connectedCities)
 			if(temp.equals(c))

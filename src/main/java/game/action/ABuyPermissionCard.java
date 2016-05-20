cf2422ba21c361f51f22104362525bc6eb0a3cf5
GianBra <gianpaolo@protonmail.com>
2016-05-20 16:51:13 +0200

    fixed. for real.

--- src/main/java/game/action/ABuyPermissionCard.java
+++ src/main/java/game/action/ABuyPermissionCard.java
@@ -1,9 +1,8 @@
 package game.action;
 
-import java.awt.Color;
 import java.util.List;
 
-import game.board.Council;
+import game.board.council.Council;
 import game.player.PermissionCard;
 import game.player.Player;
 import game.player.PoliticCard;
@@ -25,32 +24,8 @@ public class ABuyPermissionCard extends Action{
 	
 	@Override
 	public void execute() throws IllegalActionException {
-		List<Color> councilorColor = council.getCouncilorsColor();
-
-		if(politicCards.size() > councilorColor.size()) {
-			throw new IllegalActionException("you are using too many cards!");
-		}
 		
-		int satisfiedCouncilors = 0;
-		
-		int multicoloredCards = filterColoredCard(politicCards);
-		for(Color c: councilorColor) {
-			if(politicCards.isEmpty()) {
-				break;
-			}
-			
-			boolean match = removeColoredCard(politicCards, c);
-			if(match) {
-				satisfiedCouncilors++;
-			}
-		}
-		
-		if(!politicCards.isEmpty()) {
-			throw new IllegalActionException("some cards are wrong, or you gave no one");
-
-		}
-		
-		int price = calculatePrice(councilorColor.size(), satisfiedCouncilors, multicoloredCards);	
+		int price = council.calculatePrice(politicCards);
 		if(player.getCoins().getAmount() < price) {
 			throw new IllegalActionException("you can not afford it!");
 		}
@@ -58,41 +33,5 @@ public class ABuyPermissionCard extends Action{
 		player.getCoins().decrease(price);
 		player.getPermissionCard().add(permCard);
 		permCard.getCardReward().assignBonusTo(player);
-	}
-	
-	private int filterColoredCard(List<PoliticCard> lp) {
-		int multicoloredCards = 0;
-		
-		for(PoliticCard card: lp) {
-			if(card.isMultipleColor()) {
-				lp.remove(card);
-				multicoloredCards++;
-			}
-		} 
-		return multicoloredCards;
-	}
-	
-	private boolean removeColoredCard(List<PoliticCard> lp, Color c) {
-		boolean match = false;
-		for(PoliticCard card: lp) {
-			if(c.equals(card.getCardColor())) {
-				lp.remove(card);
-				match = true;
-				break;
-			}
-		}
-		return match;
-	}
-	
-	private int calculatePrice(int councSize, int satisfiedCouncilors, int multicoloredCards) {
-		int price;
-		int unsatisfiedCouncilors = councSize - satisfiedCouncilors - multicoloredCards;
-		if(unsatisfiedCouncilors == 0) {
-			price = multicoloredCards;
-		} else {
-			price = multicoloredCards + unsatisfiedCouncilors * 3 + 1;
-		}
-		
-		return price;
 	}
 }
--- src/main/java/game/board/council/Council.java
+++ src/main/java/game/board/council/Council.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
+import game.action.IllegalActionException;
+import game.player.PoliticCard;
+
 
 public class Council {
 	private List<Councilor> councMembers;
@@ -48,5 +51,68 @@ public class Council {
 		return councMembers.get(0).getColor();
 	}
 	
+	public int calculatePrice(List<PoliticCard> politicCards) throws IllegalActionException{
+		List<Color> councilorColor = this.getCouncilorsColor();
+		
+		if(politicCards.size() > councilorColor.size()) {
+			throw new IllegalActionException("you are using too many cards!");
+		}
+		
+		int satisfiedCouncilors = 0;
+		
+		int multicoloredCards = filterColoredCard(politicCards);
+		for(Color c: councilorColor) {
+			if(politicCards.isEmpty()) {
+				break;
+			}
+			
+			boolean match = removeColoredCard(politicCards, c);
+			if(match) {
+				satisfiedCouncilors++;
+			}
+		}
+		
+		if(!politicCards.isEmpty()) {
+			throw new IllegalActionException("some cards are wrong, or you gave no one");
+
+		}
+		
+		return calculatePrice(councilorColor.size(), satisfiedCouncilors, multicoloredCards);			
+	}
 	
+	private int filterColoredCard(List<PoliticCard> lp) {
+		int multicoloredCards = 0;
+		
+		for(PoliticCard card: lp) {
+			if(card.isMultipleColor()) {
+				lp.remove(card);
+				multicoloredCards++;
+			}
+		} 
+		return multicoloredCards;
+	}
+	
+	private boolean removeColoredCard(List<PoliticCard> lp, Color c) {
+		boolean match = false;
+		for(PoliticCard card: lp) {
+			if(c.equals(card.getCardColor())) {
+				lp.remove(card);
+				match = true;
+				break;
+			}
+		}
+		return match;
+	}
+	
+	private int calculatePrice(int councSize, int satisfiedCouncilors, int multicoloredCards) {
+		int price;
+		int unsatisfiedCouncilors = councSize - satisfiedCouncilors - multicoloredCards;
+		if(unsatisfiedCouncilors == 0) {
+			price = multicoloredCards;
+		} else {
+			price = multicoloredCards + unsatisfiedCouncilors * 3 + 1;
+		}
+		
+		return price;
+	}
 }

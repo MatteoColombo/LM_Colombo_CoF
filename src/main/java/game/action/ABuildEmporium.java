1c45aebafb899eeccd9c2b2f4b9988d91b13474a
Matteo Colombo <matte.colo@gmail.com>
2016-05-21 17:42:28 +0200

    Rewards implemented with map explorer

--- src/main/java/game/action/ABuildEmporium.java
+++ src/main/java/game/action/ABuildEmporium.java
@@ -1,46 +1,60 @@
 package game.action;
 
+import java.util.List;
+
+import game.board.MapExplorer;
 import game.board.city.City;
 import game.exceptions.IllegalActionException;
 import game.player.Emporium;
 import game.player.PermissionCard;
 import game.player.Player;
+import game.reward.Reward;
 
-public class ABuildEmporium extends Action{
+public class ABuildEmporium extends Action {
 	private Player player;
 	private Emporium emporium;
 	private PermissionCard permissionCard;
 	private City city;
-	
+
 	public ABuildEmporium(Player p, PermissionCard permissionCard, City city) {
 		super(true);
 		this.player = p;
 		this.permissionCard = permissionCard;
 		this.city = city;
 	}
-	
+
 	@Override
 	public void execute() throws IllegalActionException {
-		
-		if(!permissionCard.getCardCity().contains(city)) {
+
+		if (!permissionCard.getCardCity().contains(city)) {
 			throw new IllegalActionException("the chosen city is invalid");
 		}
-		
-		if(city.hasEmporiumOfPlayer(player)) {
+
+		if (city.hasEmporiumOfPlayer(player)) {
 			throw new IllegalActionException("you already have an emporium there");
 		}
-		
-		if(city.getNumberOfEmporium() > 0) {
-			if(player.getAssistants().getAmount() < city.getNumberOfEmporium()) {
+
+		if (city.getNumberOfEmporium() > 0) {
+			if (player.getAssistants().getAmount() < city.getNumberOfEmporium()) {
 				throw new IllegalActionException("you can not afford it!");
 			}
 			player.getAssistants().decrease(city.getNumberOfEmporium());
 		}
-		
+
+		assignEmporium();
+		assignRewards();
+	}
+
+	private void assignEmporium(){
 		this.emporium.setCity(city);
-		
-		// TODO get rewards from map exploration
-		
 	}
 	
+	private void assignRewards() {
+		MapExplorer explorer = new MapExplorer();
+		List<Reward> rewards = explorer.getAdiacentRewards(this.city, this.player);
+		for (Reward rew : rewards) {
+			rew.assignBonusTo(this.player);
+		}
+	}
+
 }

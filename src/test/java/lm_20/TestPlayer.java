372b4a5c8d19ebd3c8f3b5198b3ccacc4d979e6f
Matteo Colombo <matte.colo@gmail.com>
2016-05-22 15:05:36 +0200

    Added more tests

--- src/test/java/lm_20/TestPlayer.java
+++ src/test/java/lm_20/TestPlayer.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
 
 import game.player.Assistants;
 import game.player.Coins;
@@ -12,45 +13,73 @@ import game.player.NoblePoints;
 import game.player.Player;
 import game.player.VictoryPoints;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestPlayer {
 
-	@Test
-	public void test() {
-		ArrayList<Color> colorList = new ArrayList<Color>();
+	private List<Color> colorList;
+	private Player p;
+
+	/**
+	 * Creates a list with the colors and it initializes the player
+	 */
+	@Before
+	public void setUp() {
+		colorList = new ArrayList<Color>();
 		colorList.add(new Color(20, 30, 40));
 		colorList.add(new Color(100, 30, 50));
 		colorList.add(new Color(200, 130, 140));
 		colorList.add(new Color(2, 3, 40));
 		colorList.add(new Color(2, 3, 4));
 		colorList.add(new Color(255, 255, 255));
-		
-		
-		Player p= new Player(10,1,6,10, colorList,0,0);
-		
-		Coins c= p.getCoins();
+
+		p = new Player(10, 1, 6, 10, colorList, 0, 0);
+	}
+
+	@Test
+	public void testCoins() {
+		Coins c = p.getCoins();
 		c.increment(5);
 		c.decrease(3);
 		assertEquals(12, c.getAmount());
-		
-		Assistants a= p.getAssistants();
+	}
+
+	@Test
+	public void testAssistants() {
+		Assistants a = p.getAssistants();
 		a.increment(2);
 		a.decrease(1);
 		assertEquals(2, a.getAmount());
-		
+	}
+
+	@Test
+	public void testVictoryPoinrs() {
+
 		VictoryPoints v = p.getVictoryPoints();
 		v.increment(5);
 		assertEquals(5, v.getAmount());
-		
-		Emporium e= p.getEmporium().get(0);
+
+	}
+
+	@Test
+	public void testEmporium() {
+		Emporium e = p.getEmporium().get(0);
 		assertEquals(p, e.getPlayer());
 		assertEquals(null, e.getCity());
-		
+	}
+
+	@Test
+	public void testNoblePoint() {
 		NoblePoints n = p.getNoblePoints();
 		n.increment(1);
-		assertEquals(1,n.getAmount());
-		
+		assertEquals(1, n.getAmount());
+		n.increment(2);
+		assertEquals(3, n.getAmount());
+	}
+
+	@Test
+	public void testActions() {
 		p.doExtraAction();
 		assertEquals(true, p.getIfExtraActionDone());
 		p.doMainAction();

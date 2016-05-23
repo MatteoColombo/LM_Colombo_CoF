f656379af06d05b879643ec3659afbbd704718ef
Matteo Colombo <matte.colo@gmail.com>
2016-05-23 21:34:30 +0200

    removed unused methods

--- src/main/java/game/reward/FlagTable.java
+++ src/main/java/game/reward/FlagTable.java
@@ -2,32 +2,22 @@ package game.reward;
 
 public class FlagTable {
 	private boolean[] flags;
-	
+
 	public FlagTable(int length) {
 		flags = new boolean[length];
 		this.unflagAll();
 	}
-	
+
 	public void unflagAll() {
-		for(int i = flags.length -1; i >= 0; i--) {
-			unflag(i);
+		for (int i = flags.length - 1; i >= 0; i--) {
+			flags[i] = false;
 		}
 	}
-	
-	public void flagAll() {
-		for(int i = flags.length -1; i >= 0; i--) {
-			flag(i);
-		}
-	}
-	
+
 	public void flag(int index) {
 		flags[index] = true;
-	}	
-	
-	public void unflag(int index) {
-		flags[index] = false;
 	}
-	
+
 	public boolean isFlagged(int index) {
 		return flags[index];
 	}

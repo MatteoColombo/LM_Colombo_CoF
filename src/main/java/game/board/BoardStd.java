d3f41d00d3db980959da4a4c4489bf7fd6258414
Matteo Colombo <matte.colo@gmail.com>
2016-05-30 11:33:21 +0200

    Removed class

--- src/main/java/game/board/BoardStd.java
+++
@@ -1,9 +0,0 @@
-package game.board;
-
-import game.exceptions.MapXMLFileException;
-
-public class BoardStd extends Board{
-	public BoardStd() throws MapXMLFileException{
-		super("resources/msp.xml", 20, 6, 4, ColorConstants.getCardsColors());
-	}
-}

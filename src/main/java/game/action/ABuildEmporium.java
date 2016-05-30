46c1416b295b668c3a40b1e4a475ba1b3fefe147
Davide Cavallini <davide.cavallini@vodafone.it>
2016-05-30 17:09:31 +0200

    Added implementation of BRManager

--- src/main/java/game/action/ABuildEmporium.java
+++ src/main/java/game/action/ABuildEmporium.java
@@ -2,23 +2,28 @@ package game.action;
 
 import java.util.List;
 
+import game.board.BoardRewardsManager;
 import game.board.city.City;
 import game.board.map.MapExplorer;
 import game.exceptions.IllegalActionException;
 import game.player.PermissionCard;
 import game.player.Player;
+import game.reward.BVictoryPoints;
 import game.reward.Reward;
 
 public class ABuildEmporium extends Action {
 	private Player player;
 	private PermissionCard permissionCard;
 	private City city;
+	private BoardRewardsManager bRewardsManager;
 
-	public ABuildEmporium(Player p, PermissionCard permissionCard, City city) throws IllegalActionException{
+	public ABuildEmporium(Player p, PermissionCard permissionCard, City city, BoardRewardsManager bRewardsManager)
+			throws IllegalActionException {
 		super(true);
 		this.player = p;
 		this.permissionCard = permissionCard;
 		this.city = city;
+		this.bRewardsManager = bRewardsManager;
 		if (!permissionCard.getCardCity().contains(city)) {
 			throw new IllegalActionException("the chosen city is invalid");
 		}
@@ -26,11 +31,10 @@ public class ABuildEmporium extends Action {
 		if (city.hasEmporiumOfPlayer(player)) {
 			throw new IllegalActionException("you already have an emporium there");
 		}
-		if ((city.getNumberOfEmporium() > 0)
-			&& (player.getAssistants().getAmount() < city.getNumberOfEmporium())) {
-				throw new IllegalActionException("you can not afford it!");		
+		if ((city.getNumberOfEmporium() > 0) && (player.getAssistants().getAmount() < city.getNumberOfEmporium())) {
+			throw new IllegalActionException("you can not afford it!");
 		}
-		
+
 	}
 
 	@Override
@@ -40,14 +44,23 @@ public class ABuildEmporium extends Action {
 		assignEmporium();
 		assignRewards();
 		this.player.doMainAction();
-		
-		
+		{// TODO if (check for color)
+			if (!city.isCapital()) {
+				BVictoryPoints playerBReward = this.bRewardsManager.getBoardColorReward(city.getColor());
+				playerBReward.assignBonusTo(player);
+			}
+		}
+		if (city.getRegion().isCompleted(this.player)) {
+			BVictoryPoints playerBReward = this.bRewardsManager.getBoardRegionReward(city.getRegion());
+			playerBReward.assignBonusTo(player);
+		}
+
 	}
 
-	private void assignEmporium(){
+	private void assignEmporium() {
 		this.city.addEmporium(player.getEmporium().remove(0));
 	}
-	
+
 	private void assignRewards() {
 		MapExplorer explorer = new MapExplorer();
 		List<Reward> rewards = explorer.getAdiacentRewards(this.city, this.player);

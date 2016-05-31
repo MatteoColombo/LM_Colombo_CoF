d8c8088703456798b15f8ba4382ca471e31c72c7
Davide Cavallini <davide.cavallini@vodafone.it>
2016-05-31 15:23:12 +0200

    Finished BRManager

--- src/main/java/model/action/ABuildEmporiumWithKing.java
+++ src/main/java/model/action/ABuildEmporiumWithKing.java
@@ -13,29 +13,32 @@ import model.reward.Reward;
 
 public class ABuildEmporiumWithKing extends Action {
 	private Player player;
-	private City city;
+	private City chosenCity;
 	private King king;
 	private MapExplorer mx;
 	private int priceForMovement;
 	private final int PRICEPERROUTE = 2;
 	private BoardRewardsManager bRewardsManager;
+	private List<City> cities;
 
-	public ABuildEmporiumWithKing(Player p, King king, City city, BoardRewardsManager bRewardsManager)
-			throws IllegalActionException {
+	public ABuildEmporiumWithKing(Player p, King king, City chosenCity, List<City> cities,
+			BoardRewardsManager bRewardsManager) throws IllegalActionException {
 		super(true);
 		this.king = king;
 		this.player = p;
-		this.city = city;
+		this.chosenCity = chosenCity;
+		this.cities = cities;
 		this.mx = new MapExplorer();
 		this.bRewardsManager = bRewardsManager;
-		if (city.hasEmporiumOfPlayer(player)) {
+		if (chosenCity.hasEmporiumOfPlayer(player)) {
 			throw new IllegalActionException("you already have an emporium there");
 		}
 
-		if ((city.getNumberOfEmporium() > 0) && (player.getAssistants().getAmount() < (city.getNumberOfEmporium()))) {
+		if ((chosenCity.getNumberOfEmporium() > 0)
+				&& (player.getAssistants().getAmount() < (chosenCity.getNumberOfEmporium()))) {
 			throw new IllegalActionException("you can not afford it!");
 		}
-		this.priceForMovement = PRICEPERROUTE * mx.getDistance(king.getKingLocation(), city);
+		this.priceForMovement = PRICEPERROUTE * mx.getDistance(king.getKingLocation(), chosenCity);
 		if ((priceForMovement > 0) && (player.getCoins().getAmount() < priceForMovement)) {
 			throw new IllegalActionException("you can not afford it!");
 		}
@@ -44,32 +47,33 @@ public class ABuildEmporiumWithKing extends Action {
 
 	@Override
 	public void execute() {
-		king.moveKing(city);
-		player.getAssistants().decreaseAmount(city.getNumberOfEmporium());
+		king.moveKing(chosenCity);
+		player.getAssistants().decreaseAmount(chosenCity.getNumberOfEmporium());
 		player.getCoins().decreaseAmount(priceForMovement);
 		this.player.doMainAction();
 		assignEmporium();
 		assignRewards();
-		{// TODO if (check for color)
-			if (!city.isCapital()) {
-				BVictoryPoints playerBReward = this.bRewardsManager.getBoardColorReward(city.getColor());
+		MapExplorer mp = new MapExplorer();
+		if (mp.isColorComplete(this.player, this.chosenCity.getColor(), this.cities)) {
+			if (!chosenCity.isCapital()) {
+				BVictoryPoints playerBReward = this.bRewardsManager.getBoardColorReward(chosenCity.getColor());
 				playerBReward.assignBonusTo(player);
 			}
 		}
-		if (city.getRegion().isCompleted(this.player)) {
-			BVictoryPoints playerBReward = this.bRewardsManager.getBoardRegionReward(city.getRegion());
+		if (chosenCity.getRegion().isCompleted(this.player)) {
+			BVictoryPoints playerBReward = this.bRewardsManager.getBoardRegionReward(chosenCity.getRegion());
 			playerBReward.assignBonusTo(player);
 		}
 
 	}
 
 	private void assignEmporium() {
-		this.city.addEmporium(player.getEmporium().remove(0));
+		this.chosenCity.addEmporium(player.getEmporium().remove(0));
 	}
 
 	private void assignRewards() {
 		MapExplorer explorer = new MapExplorer();
-		List<Reward> rewards = explorer.getAdiacentRewards(this.city, this.player);
+		List<Reward> rewards = explorer.getAdiacentRewards(this.chosenCity, this.player);
 		for (Reward rew : rewards) {
 			rew.assignBonusTo(this.player);
 		}
--- src/test/java/lm_20/TestBuildEmporium.java
+++ src/test/java/lm_20/TestBuildEmporium.java
@@ -12,20 +12,29 @@ import org.junit.Test;
 
 import model.action.ABuildEmporium;
 import model.action.ABuildEmporiumWithKing;
+import model.board.BoardRewardsManager;
 import model.board.King;
 import model.board.council.CouncilorPool;
 import model.board.map.MapLoader;
 import model.exceptions.IllegalActionException;
+import model.exceptions.MapXMLFileException;
 import model.player.PermissionCard;
 import model.player.Player;
+import model.reward.BVictoryPoints;
+import model.reward.BoardColorReward;
+import model.reward.BoardRegionReward;
 
 public class TestBuildEmporium {
 	private List<Color> colorList;
 	private Player player;
 	private CouncilorPool pool;
+	private List<BoardColorReward> bColorRewards;
+	private List<BoardRegionReward> bRegionRewards;
+	private List<BVictoryPoints> bKingRewards;
+	private BoardRewardsManager bRewardManager;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws MapXMLFileException {
 		colorList = new ArrayList<Color>();
 		colorList.add(Color.BLACK);
 		colorList.add(Color.BLUE);
@@ -35,6 +44,17 @@ public class TestBuildEmporium {
 		colorList.add(Color.ORANGE);
 		this.player = new Player(10, 3, 6, 10, colorList, 0, 0);
 		this.pool = new CouncilorPool(4, 4, colorList);
+		bColorRewards = new ArrayList<>();
+		bKingRewards = new ArrayList<>();
+		bRegionRewards = new ArrayList<>();
+		bColorRewards.add(new BoardColorReward(Color.decode("#008000"), 10));
+		bColorRewards.add(new BoardColorReward(Color.decode("#2268df"), 1));
+		bColorRewards.add(new BoardColorReward(Color.decode("#ffd700"), 50));
+		bColorRewards.add(new BoardColorReward(Color.decode("#f44343"), 20));
+		MapLoader ml = new MapLoader("src/main/resources/map.xml", pool);
+		bRegionRewards.add(new BoardRegionReward(ml.getRegions().get(0), 20));
+		this.bRewardManager = new BoardRewardsManager(bColorRewards, bRegionRewards, bKingRewards);
+
 	}
 
 	/**
@@ -42,90 +62,90 @@ public class TestBuildEmporium {
 	 * after the first emporium it tries to build a second one in the same city
 	 * with the same player, the action is rejected
 	 */
-	@Test(expected=IllegalActionException.class)
+	@Test(expected = IllegalActionException.class)
 	public void testBuildWithPermission() throws Exception {
 		MapLoader ml;
 		PermissionCard card;
 		ABuildEmporium action;
-			ml = new MapLoader("src/main/resources/map.xml", pool);
-			card = new PermissionCard(ml.getRegions().get(0).getCities());
-			action = new ABuildEmporium(player, card, card.getCardCity().get(0), null);
-			assertEquals(true, action.isMain());
-			action.execute();
-			action = new ABuildEmporium(player, card, card.getCardCity().get(0), null);
+		ml = new MapLoader("src/main/resources/map.xml", pool);
+		card = new PermissionCard(ml.getRegions().get(0).getCities());
+		action = new ABuildEmporium(player, card, card.getCardCity().get(0), null, null);
+		assertEquals(true, action.isMain());
+		action.execute();
+		action = new ABuildEmporium(player, card, card.getCardCity().get(0), null, null);
 	}
 
 	/**
 	 * Tries to build an emporium in a city which doesn't satisfies the permit
 	 * card
 	 */
-	@Test(expected=IllegalActionException.class)
-	public void testBuildWithWrongCity() throws Exception{
+	@Test(expected = IllegalActionException.class)
+	public void testBuildWithWrongCity() throws Exception {
 		MapLoader ml;
 		PermissionCard card;
 		ABuildEmporium action;
-			ml = new MapLoader("src/main/resources/map.xml", pool);
-			card = new PermissionCard(ml.getRegions().get(0).getCities());
-			action = new ABuildEmporium(player, card, card.getCardCity().get(0), null);
-			action.execute();
-			action = new ABuildEmporium(player, card, ml.getRegions().get(2).getCities().get(0), null);
+		ml = new MapLoader("src/main/resources/map.xml", pool);
+		card = new PermissionCard(ml.getRegions().get(0).getCities());
+		action = new ABuildEmporium(player, card, card.getCardCity().get(0), null, null);
+		action.execute();
+		action = new ABuildEmporium(player, card, ml.getRegions().get(2).getCities().get(0), null, null);
 	}
 
 	/**
 	 * Tries to build an emporium in a city which already has another emporium
 	 * of another player. The player has no money and the action is rejected
 	 */
-	@Test(expected=IllegalActionException.class)
-	public void testBuildWithAnotherPlayerEmporium() throws Exception{
+	@Test(expected = IllegalActionException.class)
+	public void testBuildWithAnotherPlayerEmporium() throws Exception {
 		MapLoader ml;
 		PermissionCard card;
 		ABuildEmporium action;
-			ml = new MapLoader("src/main/resources/map.xml", pool);
-			card = new PermissionCard(ml.getRegions().get(0).getCities());
-			Player p2 = new Player(10, 0, 6, 10, colorList, 0, 0);
-			action = new ABuildEmporium(player, card, card.getCardCity().get(0), null);
-			action.execute();
-			action = new ABuildEmporium(p2, card, card.getCardCity().get(0), null);
-			action.execute();
+		ml = new MapLoader("src/main/resources/map.xml", pool);
+		card = new PermissionCard(ml.getRegions().get(0).getCities());
+		Player p2 = new Player(10, 0, 6, 10, colorList, 0, 0);
+		action = new ABuildEmporium(player, card, card.getCardCity().get(0), null, null);
+		action.execute();
+		action = new ABuildEmporium(p2, card, card.getCardCity().get(0), null, null);
+		action.execute();
 	}
 
 	/**
 	 * Builds an emporium using the king in a different city and then tries to
 	 * build another in the same
 	 */
-	@Test(expected=IllegalActionException.class)
+	@Test(expected = IllegalActionException.class)
 	public void testBuildWithKing() throws Exception {
 		MapLoader ml;
 		King king;
 		ABuildEmporiumWithKing action;
 
-			ml = new MapLoader("src/main/resources/map.xml", pool);
-			king = new King(ml.getKingCity(), pool.getCouncil());
-			action = new ABuildEmporiumWithKing(player, king, ml.getRegions().get(0).getCities().get(0), null);
-			assertEquals(true, action.isMain());
-			action.execute();
-			assertEquals(1, ml.getRegions().get(0).getCities().get(0).getNumberOfEmporium() );
-			action = new ABuildEmporiumWithKing(player, king, ml.getRegions().get(0).getCities().get(0), null);
-		
+		ml = new MapLoader("src/main/resources/map.xml", pool);
+		king = new King(ml.getKingCity(), pool.getCouncil());
+		action = new ABuildEmporiumWithKing(player, king, ml.getRegions().get(0).getCities().get(0), null, null);
+		assertEquals(true, action.isMain());
+		action.execute();
+		assertEquals(1, ml.getRegions().get(0).getCities().get(0).getNumberOfEmporium());
+		action = new ABuildEmporiumWithKing(player, king, ml.getRegions().get(0).getCities().get(0), null, null);
+
 	}
 
 	/**
 	 * Builds an emporium using the king in a differente city but can't afford
 	 * it
 	 */
-	@Test(expected=IllegalActionException.class)
+	@Test(expected = IllegalActionException.class)
 	public void testBuildWithKingNoMoney() throws Exception {
 		MapLoader ml;
 		King king;
 		ABuildEmporiumWithKing action;
 		this.player.getCoins().decreaseAmount(3);
-		
-			ml = new MapLoader("src/main/resources/map.xml", pool);
-			king = new King(ml.getKingCity(), pool.getCouncil());
-			action = new ABuildEmporiumWithKing(player, king, ml.getRegions().get(0).getCities().get(0), null);
-			assertEquals(true, action.isMain());
-			action.execute();
-			assertEquals(10, player.getCoins().getAmount());
+
+		ml = new MapLoader("src/main/resources/map.xml", pool);
+		king = new King(ml.getKingCity(), pool.getCouncil());
+		action = new ABuildEmporiumWithKing(player, king, ml.getRegions().get(0).getCities().get(0), null, null);
+		assertEquals(true, action.isMain());
+		action.execute();
+		assertEquals(10, player.getCoins().getAmount());
 	}
 
 }

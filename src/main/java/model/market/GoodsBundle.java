1238e771533eb1b76548e6bcb1fd9e70ea7b55ed
Davide Cavallini <davide.cavallini@vodafone.it>
2016-06-06 20:36:22 +0200

    Finished Market Sub-Classes initialization

--- src/main/java/model/market/GoodsBundle.java
+++ src/main/java/model/market/GoodsBundle.java
@@ -1,12 +1,73 @@
 package model.market;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.player.*;
 
 public class GoodsBundle {
-	private Player player;
+	private Player playerOwner;
 	private List<MPermissionCard> sellingPermissionCards;
 	private List<MPoliticCard> sellingPoliticCards;
-	private List<MAssistants> sellingAssistants;
+	private MAssistants sellingAssistants;
+
+	public GoodsBundle(Player playerOwner) {
+		this.playerOwner = playerOwner;
+		this.sellingPermissionCards = new ArrayList<>();
+		this.sellingPoliticCards = new ArrayList<>();
+		this.sellingAssistants = new MAssistants(0, 0);
+	}
+
+	public Player getPlayerOwner() {
+		return this.playerOwner;
+	}
+
+	public List<MPermissionCard> getSellingPermissionCards() {
+		return this.sellingPermissionCards;
+	}
+
+	public List<MPoliticCard> getSellingPoliticCards() {
+		return this.sellingPoliticCards;
+	}
+
+	public void setSellingAssistants(int amountOfSellingAssistants, int askedPriceForEachOne) {
+		int previousAssistans = this.sellingAssistants.getSellingAssistants().getAmount();
+		int previousPrice = this.sellingAssistants.getPrice().getAmount();
+		this.sellingAssistants.getSellingAssistants().decreaseAmount(previousAssistans);
+		this.sellingAssistants.getPrice().decreaseAmount(previousPrice);
+		this.sellingAssistants.getSellingAssistants().increaseAmount(amountOfSellingAssistants);
+		this.sellingAssistants.getPrice().increaseAmount(askedPriceForEachOne);
+	}
+
+	public MAssistants getSellingAssistants() {
+		return this.sellingAssistants;
+	}
+
+	public Coins getPermissionCardsPrice() {
+		int i = 0;
+		if (!this.sellingPermissionCards.isEmpty())
+			for (MPermissionCard mPermissionCard : this.sellingPermissionCards)
+				i += mPermissionCard.getPrice().getAmount();
+		return new Coins(i);
+	}
+
+	public Coins getPoliticCardsPrice() {
+		int i = 0;
+		if (!this.sellingPoliticCards.isEmpty())
+			for (MPoliticCard mPoliticCard : this.sellingPoliticCards)
+				i += mPoliticCard.getPrice().getAmount();
+		return new Coins(i);
+	}
+
+	public Coins getAssistantsPrice() {
+		return new Coins(this.sellingAssistants.getSellingAssistants().getAmount()
+				* this.sellingAssistants.getPrice().getAmount());
+	}
+
+	public Coins getBundlePrice() {
+		int i = getPoliticCardsPrice().getAmount() + getPermissionCardsPrice().getAmount()
+				+ getAssistantsPrice().getAmount();
+		return new Coins(i);
+	}
+
 }
--- src/main/java/model/market/Market.java
+++ src/main/java/model/market/Market.java
@@ -1,6 +1,18 @@
 package model.market;
 
+import java.util.List;
+
+import model.player.*;
+
 public class Market {
-	
+	private List<Player> allPlayers;
+
+	public Market(List<Player> allPlayers) {
+		this.allPlayers = allPlayers;
+	}
+
+	public List<Player> getAllPlayers() {
+		return allPlayers;
+	}
 
 }

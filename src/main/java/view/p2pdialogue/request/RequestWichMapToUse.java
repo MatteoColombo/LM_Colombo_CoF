8a5ce9f8bc213cbf37f19a801435113c73d8519b
Matteo Colombo <matte.colo@gmail.com>
2016-06-17 14:12:38 +0200

    bugfix

--- src/main/java/view/p2pdialogue/request/RequestWichMapToUse.java
+++ src/main/java/view/p2pdialogue/request/RequestWichMapToUse.java
@@ -1,22 +1,15 @@
 package view.p2pdialogue.request;
 
 import java.util.List;
-
 import client.view.ViewInterface;
-import view.p2pdialogue.combinedrequest.CombinedDialogue;
 
 public class RequestWichMapToUse implements Request {
 
 	private static final long serialVersionUID = 3660956964273763957L;
 
-	private List<String> maps;
-	
-	public RequestWichMapToUse(List<String> maps) {
-		this.maps=maps;
-	}
 	@Override
 	public void execute(ViewInterface view) {
-		view.printAskWhichMapToUse(maps);
+		view.printAskWhichMapToUse();
 	}
 
 }
